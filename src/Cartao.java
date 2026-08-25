6ee3e710dc0d87546e2a8e74c9a98853a5e0b105
yCels <celso5mf@gmail.com>
2026-08-25 19:55:30 -0300

    feat funcao em cartao para permitir compra baseado no saldo do cartao

--- src/Cartao.java
+++ src/Cartao.java
@@ -5,6 +5,17 @@ public class Cartao {
     private double limite;
     private ArrayList<Compra> compras = new ArrayList<Compra>();
 
+
+    public String lancaCompra(Compra compra){
+        if (this.saldo >= compra.getPreco()){
+            this.saldo -= compra.getPreco();
+            this.compras.add(compra);
+            return "Compra realizada com sucesso!";
+        }else {
+            return "Compra negada saldo insuficiente!";
+        }
+    }
+
     public Cartao(double saldo, double limite, ArrayList<Compra> compras){
         this.saldo = saldo;
         this.limite = saldo;
