71afb0bfe50c836d4b635f4e215d371f574e17b9
yCels <celso5mf@gmail.com>
2026-08-25 19:41:57 -0300

    feat : modelagem da classe cartao

---
+++ src/Cartao.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+
+public class Cartao {
+    private double saldo;
+    private double limite;
+    private ArrayList<Compra> compras = new ArrayList<Compra>();
+
+    public Cartao(double saldo, double limite, ArrayList<Compra> compras){
+        this.saldo = saldo;
+        this.limite = saldo;
+        this.compras = new ArrayList<>();
+    }
+
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public double getLimite() {
+        return limite;
+    }
+
+    public void setLimite(double limite) {
+        this.limite = limite;
+    }
+
+    public ArrayList<Compra> getCompras() {
+        return compras;
+    }
+
+    public void setCompras(ArrayList<Compra> compras) {
+        this.compras = compras;
+    }
+
+
+}
---
+++ src/Compra.java
@@ -0,0 +1,4 @@
+public class Compra {
+    private String nome;
+    private double preco;
+}
