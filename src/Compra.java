8f9f1e8df40e41b491a80b91608ed049cea69024
yCels <celso5mf@gmail.com>
2026-08-25 19:51:42 -0300

    feat:modelgaem da classe compra

--- src/Compra.java
+++ src/Compra.java
@@ -1,4 +1,22 @@
 public class Compra {
     private String nome;
     private double preco;
+
+    public Compra(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    @Override
+    public String toString() {
+        return  "A compra de : " + this.nome + "foi no valor de: " + this.preco;
+    }
 }
